966bdbaf7af093691a8792f9e54dc34d5395d475
Nicolas <charvier.nicolas@gmail.com>
2022-06-27 11:43:33 +0200

    Revert "Update Facebook provider with good data"
    This reverts commit 09a979c338efd421939bf43857db0892126c0544.

--- client/index.php
+++ client/index.php
@@ -16,21 +16,19 @@ function login()
         "response_type"=>"code",
         "redirect_uri"=>"http://localhost:8081/oauth_success",
     ]);
-
     echo "
         <form method=\"POST\" action=\"/oauth_success\">
             <input type=\"text\" name=\"username\"/>
             <input type=\"password\" name=\"password\"/>
             <input type=\"submit\" value=\"Login\"/>
-        </form>";
-
+        </form>
+    ";
     $fbQueryParams = http_build_query([
         "state"=>bin2hex(random_bytes(16)),
         "client_id"=> FB_CLIENT_ID,
         "scope"=>"public_profile,email",
         "redirect_uri"=>"https://localhost/fb_oauth_success",
     ]);
-
     echo "<a href=\"http://localhost:8080/auth?{$queryParams}\">Login with Oauth-Server</a><br>";
     echo "<a href=\"https://www.facebook.com/v13.0/dialog/oauth?{$fbQueryParams}\">Login with Facebook</a>";
 }
