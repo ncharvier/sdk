90aa58d85d11db05671ddf515571c8d2cb8478e0
Nicolas <67713953+tshadow42@users.noreply.github.com>
2022-06-27 11:38:27 +0200

    Update Facebook provider with good data
    Split provider in one file by providers

--- client/index.php
+++ client/index.php
@@ -1,9 +1,10 @@
 <?php
 
-define("CLIENT_ID", '67dc2be521bec2ff862d3ab057de216b');
-define("FB_CLIENT_ID", '1311135729390173');
-define("CLIENT_SECRET", '04054cf433eeb3976252c81b6d657fda');
-define("FB_CLIENT_SECRET", 'fc5e25661fe961ab85d130779357541e');
+require "Providers/OauthServerProvider.php";
+require "Providers/FacebookProvider.php";
+
+define("FB_CLIENT_ID", '673068700397624');
+define("FB_CLIENT_SECRET", '613d2a9c61783d2b6ebb6ff86fec960e');
 
 // Create a login page with a link to oauth
 function login()
@@ -32,112 +33,16 @@ function login()
     echo "<a href=\"https://www.facebook.com/v13.0/dialog/oauth?{$fbQueryParams}\">Login with Facebook</a>";
 }
 
-// get token from code then get user info
-function callback()
-{
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        ["username"=> $username, "password" => $password] = $_POST;
-        $specifParams = [
-            "grant_type" => "password",
-            "username" => $username,
-            "password" => $password,
-       ];
-    } else {
-        ["code"=> $code, "state" => $state] = $_GET;
-        $specifParams = [
-            "grant_type" => "authorization_code",
-            "code" => $code
-        ];
-    }
-    $queryParams = http_build_query(array_merge(
-        $specifParams,
-        [
-            "redirect_uri" => "http://localhost:8081/oauth_success",
-            "client_id" => CLIENT_ID,
-            "client_secret" => CLIENT_SECRET,
-        ]
-    ));
-    $response = file_get_contents("http://server:8080/token?{$queryParams}");
-    if (!$response) {
-        echo $http_response_header;
-        return;
-    }
-    ["access_token" => $token] = json_decode($response, true);
-
-
-    $context = stream_context_create([
-        "http"=>[
-            "header"=>"Authorization: Bearer {$token}"
-        ]
-    ]);
-    $response = file_get_contents("http://server:8080/me", false, $context);
-    if (!$response) {
-        echo $http_response_header;
-        return;
-    }
-    var_dump(json_decode($response, true));
-}
-
-// Facebook oauth: exchange code with token then get user info
-function fbcallback()
-{
-    $token = getToken("https://graph.facebook.com/v13.0/oauth/access_token", FB_CLIENT_ID, FB_CLIENT_SECRET);
-    $user = getFbUser($token);
-    $unifiedUser = (fn () => [
-        "id" => $user["id"],
-        "name" => $user["name"],
-        "email" => $user["email"],
-        "firstName" => $user['first_name'],
-        "lastName" => $user['last_name'],
-    ])();
-    var_dump($unifiedUser);
-}
-function getFbUser($token)
-{
-    $context = stream_context_create([
-        "http"=>[
-            "header"=>"Authorization: Bearer {$token}"
-        ]
-    ]);
-    $response = file_get_contents("https://graph.facebook.com/v13.0/me?fields=last_name,first_name,email", false, $context);
-    if (!$response) {
-        echo $http_response_header;
-        return;
-    }
-    return json_decode($response, true);
-}
-function getToken($baseUrl, $clientId, $clientSecret)
-{
-    ["code"=> $code, "state" => $state] = $_GET;
-    $queryParams = http_build_query([
-        "client_id"=> $clientId,
-        "client_secret"=> $clientSecret,
-        "redirect_uri"=>"https://localhost/fb_oauth_success",
-        "code"=> $code,
-        "grant_type"=>"authorization_code",
-    ]);
-
-    $url = $baseUrl . "?{$queryParams}";
-    $response = file_get_contents($url);
-    if (!$response) {
-        echo $http_response_header;
-        return;
-    }
-    ["access_token" => $token] = json_decode($response, true);
-
-    return $token;
-}
-
 $route = $_SERVER["REQUEST_URI"];
 switch (strtok($route, "?")) {
     case '/login':
         login();
         break;
     case '/oauth_success':
-        callback();
+        OauthServerProvider::callback();
         break;
     case '/fb_oauth_success':
-        fbcallback();
+        FacebookProvider::callback();
         break;
     default:
         http_response_code(404);
