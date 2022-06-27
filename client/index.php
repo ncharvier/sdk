f1626b3d18146ddb1b2cc549e5ae4fd3f5c88d46
Nicolas <charvier.nicolas@gmail.com>
2022-06-27 15:24:58 +0200

    Add bootstrap

--- client/index.php
+++ client/index.php
@@ -16,11 +16,12 @@ function login()
     ]);
 
     echo "
-        <form method=\"POST\" action=\"/oauth_success\">
-            <input type=\"text\" name=\"username\"/>
-            <input type=\"password\" name=\"password\"/>
-            <input type=\"submit\" value=\"Login\"/>
-        </form>
+        <div class='m-5'>
+            <form method=\"POST\" action=\"/oauth_success\">
+                <input class='form-control mb-2' type=\"text\" name=\"username\" placeholder='Username'/>
+                <input class='form-control mb-2' type=\"password\" name=\"password\" placeholder='Password'/>
+                <input class='btn btn-sm btn-success mb-2' type=\"submit\" value=\"Login\"/>
+            </form>
     ";
 
     $fbQueryParams = http_build_query([
@@ -37,25 +38,45 @@ function login()
         "redirect_uri"=>"https://localhost/gh_oauth_success",
     ]);
 
-    echo "<a href=\"http://localhost:8080/auth?{$queryParams}\">Login with Oauth-Server</a><br>";
-    echo "<a href=\"https://www.facebook.com/v13.0/dialog/oauth?{$fbQueryParams}\">Login with Facebook</a><br>";
-    echo "<a href=\"https://github.com/login/oauth/authorize?{$ghQueryParams}\">Login with Github</a><br>";
-}
+    echo "<a class='btn btn-sm btn-primary mb-2' href=\"http://localhost:8080/auth?{$queryParams}\">Login with Oauth-Server</a><br>";
+    echo "<a class='btn btn-sm btn-primary mb-2' href=\"https://www.facebook.com/v13.0/dialog/oauth?{$fbQueryParams}\">Login with Facebook</a><br>";
+    echo "<a class='btn btn-sm btn-primary mb-2' href=\"https://github.com/login/oauth/authorize?{$ghQueryParams}\">Login with Github</a><br>";
 
-$route = $_SERVER["REQUEST_URI"];
-switch (strtok($route, "?")) {
-    case '/login':
-        login();
-        break;
-    case '/oauth_success':
-        OauthServerProvider::callback();
-        break;
-    case '/fb_oauth_success':
-        FacebookProvider::callback();
-        break;
-    case '/gh_oauth_success':
-        GithubProvider::callback();
-        break;
-    default:
-        http_response_code(404);
+    echo "</div>";
 }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>OAuth2 - connection hub</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <?php
+        $route = $_SERVER["REQUEST_URI"];
+        switch (strtok($route, "?")) {
+            case '/login':
+                login();
+                break;
+            case '/oauth_success':
+                OauthServerProvider::callback();
+                break;
+            case '/fb_oauth_success':
+                FacebookProvider::callback();
+                break;
+            case '/gh_oauth_success':
+                GithubProvider::callback();
+                break;
+            default:
+                http_response_code(404);
+        }
+        ?>
+    </body>
+</html>
+
+
